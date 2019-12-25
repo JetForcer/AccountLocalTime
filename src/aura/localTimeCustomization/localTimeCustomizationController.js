af2df0dcf5a506fa5d759c72b0b8eb6a86e3134a
Dmitry Pilipets <dmitriy.pilipets@gmail.com>
2019-12-25 15:20:00 +0200

    improve customization form

--- src/aura/localTimeCustomization/localTimeCustomizationController.js
+++ src/aura/localTimeCustomization/localTimeCustomizationController.js
@@ -1,79 +1,51 @@
 ({
-    doInit: function (component) {
-        function initUseGeolocation() {
-            let action = component.get('c.isUseGeolocation');
-            action.setParams({"opportunityId": component.get("v.recordId")});
-            action.setCallback(this, function (response) {
-                let returnValue = response.getReturnValue();
-                component.set("v.useGeolocation", returnValue);
-            });
-            $A.enqueueAction(action);
-        }
+    doInit: function(component) {
+        let saveSetting = component.get('c.getOrCreateSetting');
+        saveSetting.setParams({"opportunityId": component.get("v.recordId")});
+        saveSetting.setCallback(self, function(a) {
+            component.set("v.settingRecord", a.getReturnValue())
+        });
+        $A.enqueueAction(saveSetting);
+    },
 
-        initUseGeolocation.call(this);
+    handleLoad: function(component) {
+        component.set('v.showSpinner', false);
+    },
 
-        function initUse12HoursFormat() {
-            let action = component.get('c.isUse12HoursFormat');
-            action.setParams({"opportunityId": component.get("v.recordId")});
-            action.setCallback(this, function (response) {
-                let returnValue = response.getReturnValue();
-                component.set("v.use12HoursFormat", returnValue);
-            });
-            $A.enqueueAction(action);
-        }
+    handleSubmit: function(component) {
+        component.set('v.disabled', true);
+        component.set('v.showSpinner', true);
 
-        initUse12HoursFormat.call(this);
+        let fetchAndInsert = component.get('c.fetchAndInsertGeoData');
+        fetchAndInsert.setParams({"opportunityId": component.get("v.recordId")});
+        $A.enqueueAction(fetchAndInsert);
 
-        function initData() {
-            let action = component.get('c.initData');
-            action.setParams({"opportunityId": component.get("v.recordId")});
-            $A.enqueueAction(action);
-        }
+        $A.get("e.force:closeQuickAction").fire();
+    },
 
-        initData.call(this);
+    handleError: function(component) {
+        component.set('v.showSpinner', false);
+
+        let toastEvent = $A.get("e.force:showToast");
+        toastEvent.setParams({
+            "title": "Error!",
+            "message": "The unknown problem."
+        });
+        toastEvent.fire();
+    },
+
+    handleSuccess: function(component) {
+        component.set('v.showSpinner', false);
+
+        let toastEvent = $A.get("e.force:showToast");
+        toastEvent.setParams({
+            "title": "The customization Saved!",
+            "message": "The Account local time customization has been Saved successfully."
+        });
+        toastEvent.fire();
     },
 
     handleCancel: function (component) {
         $A.get("e.force:closeQuickAction").fire();
-    },
-
-    saveOpportunity: function (component) {
-        let action = component.get("c.updateOpportunityRecord");
-        action.setParams({
-            "opportunityId": component.get("v.recordId"),
-            "useGeolocation": component.get("v.useGeolocation"),
-            "use12HoursFormat": component.get("v.use12HoursFormat")
-        });
-        action.setCallback(this, function (response) {
-            let state = response.getState();
-            let error = response.getError();
-
-            if (state === "SUCCESS" || state === "DRAFT") {
-                component.set("v.opportunityRecord", response.getReturnValue());
-
-                // Success! Prepare a toast UI message
-                let resultsToast = $A.get("e.force:showToast");
-                resultsToast.setParams({
-                    "title": "The customization Saved",
-                    "message": "The Account local time field customization successfully saved."
-                });
-
-                // Update the UI: close panel, show toast, refresh account page
-                $A.get("e.force:closeQuickAction").fire();
-                resultsToast.fire();
-
-                // Reload the view so components not using force:recordData
-                // are updated
-                $A.get("e.force:refreshView").fire();
-            } else if (state === "INCOMPLETE") {
-                console.log("User is offline, device doesn't support drafts.");
-            } else if (state === "ERROR") {
-                console.log('Problem saving contact, error: ' + JSON.stringify(error));
-            } else {
-                console.log('The unknown problem, state: ' + state + ', error: ' + JSON.stringify(error));
-            }
-        });
-
-        $A.enqueueAction(action);
     }
-})
+});
