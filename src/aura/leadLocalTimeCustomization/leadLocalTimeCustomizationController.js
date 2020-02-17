2100889fcf69998a48760646fac3f1e26c66d975
Dmitry Pilipets <dmitriy.pilipets@gmail.com>
2020-02-17 17:22:29 +0200

    add  managed package namespace prefix

--- src/aura/leadLocalTimeCustomization/leadLocalTimeCustomizationController.js
+++ src/aura/leadLocalTimeCustomization/leadLocalTimeCustomizationController.js
@@ -2,6 +2,6 @@
     doInit: function (component) {
         let recordId = component.get("v.recordId");
         let service = component.find('service');
-        service.doInit(recordId, 'Lead', 'Lead_Local_Time_Setting__c', 'Local time')
+        service.doInit(recordId, 'Lead', 'localTimeJF__Lead_Local_Time_Setting__c', 'Local time')
     }
 });
--- src/aura/opportunityLocalTimeCustomization/opportunityLocalTimeCustomizationController.js
+++ src/aura/opportunityLocalTimeCustomization/opportunityLocalTimeCustomizationController.js
@@ -2,6 +2,6 @@
     doInit: function (component) {
         let recordId = component.get("v.recordId");
         let service = component.find('service');
-        service.doInit(recordId, 'Opportunity', 'Opportunity_Local_Time_Setting__c', 'Account local time')
+        service.doInit(recordId, 'Opportunity', 'localTimeJF__Opportunity_Local_Time_Setting__c', 'Account local time')
     }
 });
